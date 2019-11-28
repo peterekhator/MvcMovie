b08b4df2cad9ca1cb488fda3cbd38199b2225fb3
peterekhator <peterekhator@gmail.com>
2019-11-28 13:18:25 -0600

    push

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = $"Hello, {name}";
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+            //return HtmlEncoder.Default.Encode($"Hello {name}, Lastname {lastname} NumTimes is: {numTimes}");
+        }
+    }
+}
